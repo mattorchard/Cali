dc40c28f5a7b19811e66cf20ed35a10dcf6ef62f
Quang-Tri <qtrido@gmail.com>
2017-07-13 10:47:26 -0400

    Got round color image xml layout for course view

--- app/src/main/java/com/example/uottawa/cali/NotificationManager.java
+++ app/src/main/java/com/example/uottawa/cali/NotificationManager.java
@@ -1,9 +1,68 @@
 package com.example.uottawa.cali;
 
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
 /**
  * Created by Quang-Tri on 12/07/2017.
  */
 
-public class NotificationManager {
+public class NotificationManager extends IntentService {
+    private static final int NOTIFICATION_ID = 3;
 
+
+    public NotificationManager() {
+        super("MyNewIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle("My Title");
+        builder.setContentText("This is the Body");
+        builder.setSmallIcon(R.drawable.ic_menu_gallery);
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 2, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //to be able to launch your activity from the notification
+        builder.setContentIntent(pendingIntent);
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("A title")
+                .setContentText("Some notification text")
+                .setSmallIcon(R.drawable.ic_menu_gallery)
+                .build();
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(NOTIFICATION_ID, notification);
+    }
+
+/*    This part of the code checks uses sharedpreferences to save the alarm preferences.
+      Copied from stack overflow so will need some changes*/
+
+/*      SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (!prefs.getBoolean("firstTime", false)) {
+
+        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+
+        AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 7);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 1);
+
+        manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY, pendingIntent);
+
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("firstTime", true);
+        editor.apply();
+    }*/
 }
