b6e7bcca593f5d15f9b1f71bba7aabb21706b662
0Calories <aanan@uottawa.ca>
2017-07-16 12:44:30 -0400

    ~File attaching and link adding + more
    -Made "Add File" clickable and navigate to file choosing activity
    -Created LinearLayout to dynamically add clickable TextViews to AssignmentActivity
    -Can now attach assignments and links
    -Created dialog for creating a new link
    -Added file reading permission to the manifest
    -Slight changes in strings.xml
    -Made primaryColor and colorAccent not ugly

--- app/src/main/java/com/example/uottawa/cali/AssignmentActivity.java
+++ app/src/main/java/com/example/uottawa/cali/AssignmentActivity.java
@@ -7,18 +7,39 @@ import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.OpenableColumns;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.io.File;
+import java.util.LinkedList;
 
 
 public class AssignmentActivity extends AppCompatActivity {
@@ -34,10 +55,21 @@ public class AssignmentActivity extends AppCompatActivity {
     private EditText descriptionEditText;
 
 
+    private LinearLayout attachmentLayout;
+    private LinearLayout linkLayout;
+    private static LinkedList<Uri> uriList;
+    private static LinkedList<String> urlList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment);
+
+        attachmentLayout = (LinearLayout) findViewById(R.id.attachmentLayout);
+        linkLayout = (LinearLayout) findViewById(R.id.linkLayout);
+        uriList = new LinkedList<>();
+        urlList = new LinkedList<>();
+
         Intent intent = getIntent();
         fresh = intent.getBooleanExtra(getString(R.string.intent_assignment_fresh), false);
         assignment = (Assignment)intent.getSerializableExtra(getString(R.string.intent_assignment_data_send));
@@ -52,7 +84,6 @@ public class AssignmentActivity extends AppCompatActivity {
         courseBackground = (LinearLayout)findViewById(R.id.courseBackgroundAssignment);
         courseBackground.setBackgroundResource(assignment.getCourse().getColorIndex());
         //Assignment name
-
         nameEditText = (EditText)findViewById(R.id.nameEditTextAssignment);
         nameEditText.setText(assignment.getName());
         //Complete
@@ -85,6 +116,8 @@ public class AssignmentActivity extends AppCompatActivity {
         descriptionEditText = (EditText)findViewById(R.id.descriptionEditTextAssignment);
         descriptionEditText.setText(assignment.getDescription());
     }
+
+
 
     @Override
     public void onBackPressed() {
@@ -136,11 +169,57 @@ public class AssignmentActivity extends AppCompatActivity {
     }
 
     public void addLink(View v){
-        Toast.makeText(getBaseContext(), "Add link feature unimplimented", Toast.LENGTH_SHORT).show();
+        //Create a new dialog to take the URL info from the user
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View dialogView = inflater.inflate(R.layout.link_dialog_layout, null);
+        final EditText urlEditText = (EditText)dialogView.findViewById(R.id.urlEditText);
+        final EditText nameEditText = (EditText)dialogView.findViewById(R.id.nameEditText);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add a new link");
+        builder.setView(dialogView);
+        AlertDialog alertDialog = builder.create();
+
+        //When OK is pressed, create a new TextView and place it in the layout
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                urlList.add(urlEditText.getText().toString());
+                TextView urlView = new TextView(AssignmentActivity.this);
+                SpannableString urlText = new SpannableString(nameEditText.getText().toString());
+                //Make the text underlined
+                urlText.setSpan(new UnderlineSpan(), 0, urlText.length(), 0);
+                urlView.setText(urlText);
+
+                //Add an onClickListener so the hyperlink can navigate to the link when clicked
+                urlView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlEditText.getText().toString()));
+                        startActivity(browserIntent);
+                    }
+                });
+
+                appendNewTextView(linkLayout, urlView);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        builder.show();
     }
 
     public void addFile(View v){
-        Toast.makeText(getBaseContext(), "Add file feature unimplemented", Toast.LENGTH_SHORT).show();
+        Intent intentAddFile = new Intent();
+        intentAddFile.setType("*/*");
+        intentAddFile.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(intentAddFile, "Select a file"), 420);
     }
 
     public void changePriority(View v) {
@@ -167,4 +246,84 @@ public class AssignmentActivity extends AppCompatActivity {
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.showSoftInput(et, InputMethodManager.SHOW_IMPLICIT);
     }
+
+    //Allows the system to get the URI of the file that the user chose
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //If the user sucessfully chose a file, add it to the list
+        if(requestCode == 420 && resultCode == RESULT_OK) {
+            final Uri selectedFile = data.getData();
+            uriList.add(selectedFile);
+
+            TextView uriView = new TextView(this);
+            uriView.setText(getFileName(selectedFile));
+            uriView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent openFileIntent = new Intent(Intent.ACTION_VIEW);
+                    openFileIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    //openFileIntent.setDataAndType(selectedFile, "*/*");
+                    openFileIntent.setData(selectedFile);
+                    try {
+                        startActivity(openFileIntent);
+                    } catch (ActivityNotFoundException e) {
+
+                    }
+                }
+            });
+            appendNewTextView(attachmentLayout, uriView);
+        }
+    }
+
+    /**
+     * Method used to add a new TextView to the Attachment or Link layout.
+     * @param parentLayout
+     * @param newTextView
+     */
+    private void appendNewTextView(LinearLayout parentLayout, TextView newTextView) {
+
+        int marginTopDp = 20;
+        int marginLeftDp = 32;
+
+        float density = getBaseContext().getResources().getDisplayMetrics().density;
+        //Convert dp margins to px
+        int marginTop = (int)(marginTopDp * density);
+        int marginLeft = (int)(marginLeftDp * density);
+
+        newTextView.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorAccent, null));
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(marginLeft, marginTop, 0, 0);
+        newTextView.setLayoutParams(layoutParams);
+
+        parentLayout.addView(newTextView);
+    }
+
+    //Method used to get a file name from a URI
+    //Via Stefan Haustein on StackOverflow:
+    //https://stackoverflow.com/questions/5568874/how-to-extract-the-file-name-from-uri-returned-from-intent-action-get-content
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
 }
